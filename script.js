6e08857826802992d40a91851af01ff832a698c0
fariyinn <fariyinn@gmail.com>
2022-07-23 16:16:12 -0400

    Add planning comments identifying subproblems

---
+++ script.js
@@ -0,0 +1,25 @@
+// Link container div for grid from HTML
+// Function to create square grid (16 x 16)
+// Insert into container div
+
+
+// Formatting for grid squares(outer and inner border)
+
+
+// Function with event listeners for hovering over grid squares
+
+
+// Function to change squares background color when hovered over
+
+
+// Add button that makes a pop up asking for number of squares per side for grid (max: 100)
+// Removes existing grid
+// Generate new grid in same space (max width: 960px)
+
+
+// Add button to choose rainbow pen
+// Function to change squares background color to random RGB value when hovered over
+
+
+// Add button to choose gradient to black pen
+// Function to change squares background color to 10% of black per hover, and with 10 hovers it should be fully black
